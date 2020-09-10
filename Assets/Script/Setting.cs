5f41c875cd7a39af63a734a272dc271c757a055a
Victor Yamane <yamanevictor@gmail.com>
2020-09-09 22:13:39 -0300

    Limiting FPS and vSyncCount

---
+++ Assets/Script/Setting.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Script {
+    public class Setting : MonoBehaviour {
+        private void Start() {
+            QualitySettings.vSyncCount = 1;
+            Application.targetFrameRate = 60;
+        }
+    }
+}
