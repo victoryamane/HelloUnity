d0d3efebe57ca80133bfeeb28b851913adab5da5
Victor Yamane <yamanevictor@gmail.com>
2020-10-12 21:51:36 -0300

    Add Enimies

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Player {
+    private static GameObject player;
+
+    public static GameObject GetInstance() {
+        if (player == null) {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+        return player;
+    }
+}
